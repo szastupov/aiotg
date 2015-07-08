db7701f9ff0491a536a46fbbe1eeaddee556ff73
Stepan Zastupov <stepan.zastupov@gmail.com>
2015-07-08 03:05:11 +0300

    New high-level wrapper for Telegram message

--- bot.py
+++ bot.py
@@ -3,8 +3,40 @@ import logging
 import asyncio
 import aiohttp
 
+from functools import partialmethod
+
 API_URL = "https://api.telegram.org"
 API_TIMEOUT = 60
+
+
+class TgMessage:
+    """High-level wrapper around Telegram message"""
+    def __init__(self, bot, data):
+        self.bot = bot
+        self.data = data
+
+    def reply(self, text):
+        """Reply to this message"""
+        return self.bot._send_message(
+            chat_id=self.data["chat"]["id"],
+            text=text,
+            disable_web_page_preview='true',
+            reply_to_message_id=self.data["message_id"]
+        )
+
+    @property
+    def sender(self):
+        user = self.data["from"]
+        parts = ["first_name", "last_name", "username"]
+        title = " ".join(filter(None, (user.get(part) for part in parts)))
+        return title
+
+    @property
+    def text(self):
+        return self.data["text"]
+
+    def is_group(self):
+        return "chat" in self.data and "title" in self.data["chat"]
 
 
 class TgBot:
@@ -34,14 +66,7 @@ class TgBot:
         assert response.status == 200
         return (yield from response.json())
 
-    def reply_to(self, message, text):
-        """Reply to specific message"""
-        return self.api_call(
-            'sendMessage',
-            chat_id=message["chat"]["id"],
-            text=text,
-            disable_web_page_preview='true',
-            reply_to_message_id=message["message_id"])
+    _send_message = partialmethod(api_call, "sendMessage")
 
     def command(self, regexp):
         """Decorator for registering commands
@@ -75,16 +100,18 @@ class TgBot:
 
         if "text" not in message:
             return
+
         text = message["text"].lower()
+        tgm = TgMessage(self, message)
 
         for patterns, handler in self.commands:
             m = re.search(patterns, text)
             if m:
-                return handler(message, m)
+                return handler(tgm, m)
 
         # No match, run default if it's a 1to1 chat
-        if "chat" in message and "title" not in message["chat"]:
-            return self._default(message)
+        if not tgm.is_group():
+            return self._default(tgm)
 
     @asyncio.coroutine
     def loop(self):
