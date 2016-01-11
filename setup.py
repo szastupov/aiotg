aabf166190001bfcf1507f0ecb848f29e8e49f84
Stepan Zastupov <stepan.zastupov@gmail.com>
2016-01-11 17:21:09 +0300

    Drop support for 3.4

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries'
