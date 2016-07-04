c095cc2ec8c95724d5316c2bbe2b47da05c9a319
Stepan Zastupov <stepan.zastupov@gmail.com>
2016-07-04 13:06:28 +0300

    0.7.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aiotg',
-    version='0.7.4',
+    version='0.7.5',
     description='Asynchronous Python API for building Telegram bots',
     url='https://github.com/szastupov/aiotg',
 
