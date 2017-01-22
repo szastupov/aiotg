97c40a74e5b75771a7875c1d1ddfbb6d5fec9810
shadrus <shadrus@gmail.com>
2017-01-22 21:25:09 +0300

    Added example for webhook

---
+++ examples/webhook.py
@@ -0,0 +1,12 @@
+import os
+from aiotg import Bot
+
+bot = Bot(api_token=os.environ["API_TOKEN"])
+bot.set_webhook(url='https://yourserver.com/webhook/fdhgufdgh98498gvneriug489')
+
+
+@bot.command(r"/echo (.+)")
+def echo(chat, match):
+    return chat.reply(match.group(1))
+
+bot.run()
