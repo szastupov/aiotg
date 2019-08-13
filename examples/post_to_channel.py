3656806f7842063ef9d1be43245f90def978e067
M.Furkan <byazade@yandex.com>
2019-08-13 11:13:16 +0300

    Update post_to_channel.py

--- examples/post_to_channel.py
+++ examples/post_to_channel.py
@@ -2,7 +2,7 @@ import os
 import asyncio
 from aiotg import Bot
 
-bot = Bot(os.environ["API_TOKEN"])
+bot = Bot(api_token="API_TOKEN")
 channel = bot.channel(os.environ["CHANNEL"])
 private = bot.private(os.environ["PRIVATE"])
 
